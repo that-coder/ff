20131921bdb434f2b761ea99c9d6f99acefd34d4
Jayakrishnan <thatcoder.space@gmail.com>
2023-06-15 14:16:13 +0530

    [commitHash:ad40f39]

---
+++ federation-expose.js
@@ -0,0 +1,3 @@
+export default {
+    "./ff_todoInput": "./src/remote/ff_todoInput",
+}
---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import TodoInput from './remote/ff_todoInput'
+
+export default function App() {
+  return <TodoInput />
+}
